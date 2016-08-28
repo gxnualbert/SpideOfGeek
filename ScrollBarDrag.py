d0eaf70bec5c9d4e06e8b3ea56549795a516e162
gxnualbert <albert.chenwesoft@gmail.com>
2016-08-28 17:28:49 +0800

    add note for the drag script

--- ScrollBarDrag.py
+++ ScrollBarDrag.py
@@ -1,14 +1,16 @@
 #coding=utf-8
 from selenium import webdriver
 import time
-#访问百度
+#access baidu.com
 
 driver=webdriver.Chrome()
 driver.get("http://www.baidu.com")
-#搜索
+#search selenium
 driver.find_element_by_id("kw").send_keys("selenium")
 driver.find_element_by_id("su").click()
 time.sleep(3)
-#将页面滚动条拖到底部
+
+#Please note that, when you execute the drag scroll bar sript, please make sure the browser is not 
+#loading anything,(you can sleep few minutes), or this script can not drag the browser
 
 driver.execute_script("window.scrollBy(0,750)", "")
